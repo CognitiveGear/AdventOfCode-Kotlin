1cb47ad091e8f71def7bae1c77d2ff9860b60436
Christopher Makin <cognitive.gear@gmail.com>
2022-12-01 22:16:10 +1000

    Day 1 complete

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,30 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun calorieList(input: List<String>): List<Int> {
+        return input
+            .joinToString("\n")
+            .split("\n\n")
+            .map { subString -> subString.split('\n')
+                .map {
+                    it.toInt()
+                }
+            }
+            .map { it.sum() }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1(list: List<Int>): Pair<Int, Int> {
+        val maxValue = list.max()
+        val maxIndex = list.indexOf(maxValue)
+        return Pair(maxValue, maxIndex)
+    }
+
+    fun part2(list: List<Int>): Int {
+        return list.sorted().takeLast(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val dataInput = readInput("Day01_data")
+    val calorieList = calorieList(dataInput)
+    println(part1(calorieList))
+    println(part2(calorieList))
 }
