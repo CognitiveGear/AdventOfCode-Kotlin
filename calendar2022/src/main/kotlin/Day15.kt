08e40873260d49985db344a20562fae93f59f773
Christopher Makin <cognitive.gear@gmail.com>
2022-12-15 22:25:09 +1000

    Complete day 15, setup for 16.

--- calendar2022/src/main/kotlin/Day15.kt
+++ calendar2022/src/main/kotlin/Day15.kt
@@ -1,17 +1,196 @@
+import java.lang.Integer.max
+import java.lang.Integer.min
+import kotlin.math.absoluteValue
+import kotlin.math.roundToInt
+import kotlin.math.sign
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
+
 class Day15 : AdventDay(2022, 15) {
 
+    val beacons : List<Point>
+    val sensors : List<Point>
+    val radii : Map<Point, Int>
+    val targetY : Int = 2000000
+
     init {
+        val numbers: List<Pair<Point, Point>> =
+            lines.map { line ->
+                line.grabInts().chunked(2).map { numbers ->
+                    Point(numbers.first(), numbers.last())
+                }
+            }.map {
+                it.first() to it.last()
+            }
+        sensors = numbers.map { it.first }
+        beacons = numbers.map { it.second }
+        radii = numbers.associate { it.first to (it.first l1Norm it.second) }
     }
 
+    private fun IntRange.overlaps(other: IntRange) : Boolean =
+        other.first in this || other.last in this || first in other || last in other
+
+    operator fun IntRange.contains(other: IntRange) : Boolean =
+        (other.first in this && other.last in this)
+
+    private fun MutableSet<IntRange>.normalize(other: IntRange) {
+        var insertion = other
+        var overlap : IntRange?
+        do {
+            overlap = firstOrNull {
+                it.overlaps(insertion)
+            }?.also {
+                remove(it)
+                insertion = insertion.combineOrNull(it)!!
+            }
+        } while (overlap != null)
+        add(insertion)
+    }
+
+    private fun IntRange.combineOrNull(other:  IntRange) : IntRange? =
+        when {
+            other in this -> this
+            this in other -> other
+            this.overlaps(other) -> {
+                if (first in other) {
+                    other.first..last
+                } else {
+                    first..other.last
+                }
+            }
+            else -> null
+        }
+
     override fun part1(): String {
-        return ""
+        val xRanges = sensors
+        .map { sensor ->
+            val reducedDistance = radii[sensor]!! - (sensor.y - targetY).absoluteValue
+            sensor.x to reducedDistance
+        }.filter {
+            it.second >= 0
+        }.map {
+            (it.first - it.second)..(it.first + it.second)
+        }
+        val allRanges : MutableSet<IntRange> = mutableSetOf()
+        xRanges.forEach {
+            allRanges.normalize(it)
+        }
+        var totalRangeSize = allRanges.sumOf {
+            it.last - it.first + 1
+        }
+        allRanges.forEach { range ->
+            beacons.distinct().forEach {
+                if (it.y == targetY && it.x in range) {
+                    totalRangeSize--
+                }
+            }
+            sensors.distinct().forEach {
+                if (it.y == targetY && it.x in range) {
+                    totalRangeSize--
+                }
+            }
+        }
+        return totalRangeSize.toString()
+    }
+
+    fun Point.tuning() : Long = x.toLong() * 4000000L + y.toLong()
+
+    data class Line(
+        val left : Point,
+        val right : Point
+    ) {
+        val m : Double?
+        val c : Double?
+
+        init {
+            val y1 = left.y.toDouble()
+            val y2 = right.y.toDouble()
+            val x1 = left.x.toDouble()
+            val x2 = right.x.toDouble()
+            if (x2 - x1 == 0.0) {
+                m = null
+                c = null
+            } else {
+                m = (y2 - y1) / (x2 - x1)
+                c = y2 - m * x2
+            }
+        }
+
+        fun intersect(arg: Line) : Point? {
+            if (m == null || c == null || arg.m == null || arg.c == null) {
+                return null
+            } else {
+                if (m - arg.m == 0.0) {
+                    return null
+                }
+                val x = ((arg.c - c) / (m - arg.m))
+                val y = (m * x + c)
+                val maxX = max(left.x, right.x)
+                val minX = min(left.x, right.x)
+                if (x > maxX || x < minX) {
+                    return null
+                }
+                return Point(x.roundToInt(), y.roundToInt())
+            }
+        }
     }
 
     override fun part2(): String {
-        return ""
+        val opposingPairs : List<Pair<Point, Point>> =
+            sensors.flatMap { left ->
+                sensors.filter { right ->
+                    left != right &&
+                    (left l1Norm right) == radii[left]!! + radii[right]!!
+                }.map { right ->
+                    if (left.x < right.x) {
+                        left to right
+                    } else {
+                        right to left
+                    }
+                }
+            }
+        fun List<Pair<Point, Point>>.toLineList() : List<Line> {
+            return map {
+                val (p, other) =
+                    if (radii[it.first]!! < radii[it.second]!!) {
+                        it
+                    } else {
+                        it.second to it.first
+                    }
+                Line(
+                    p + Point(0, radii[p]!! + 1),
+                    p + Point((radii[p]!! + 1) * (other.y - p.y).sign, 0)
+                )
+            }
+        }
+        val leanLeft = opposingPairs.filter {
+            it.first.y > it.second.y
+        }.toLineList()
+        val leanRight = opposingPairs.filter {
+            it.first.y < it.second.y
+        }.toLineList()
+        val intersects = mutableListOf<Point>()
+        leanLeft.forEach { left ->
+            leanRight.forEach { right ->
+                left.intersect(right)?.let {
+                    intersects.add(it)
+                }
+            }
+        }
+        intersects.removeIf {
+            it.x < 0 || it. y < 0 || it.x > 4000000 || it.y > 4000000 ||
+            sensors.any { sensor ->
+                it l1Norm sensor <= radii.getValue(sensor)
+            }
+        }
+        return intersects.first().tuning().toString()
     }
 }
 
+@OptIn(ExperimentalTime::class)
 fun main() {
-    Day15().main()
+    val time = measureTime {
+        Day15().main()
+    }
+    println(time)
 }
---
+++ calendar2022/src/main/kotlin/Day16.kt
@@ -0,0 +1,13 @@
+class Day16 : AdventDay(2022, 16) {
+    override fun part1(): String {
+        return ""
+    }
+
+    override fun part2(): String {
+        return ""
+    }
+}
+
+fun main() {
+    Day16().main()
+}
